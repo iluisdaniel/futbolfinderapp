a70504711c74b1b7d41b7b4f36f23c4062fcbebc
Luis Fonseca <luisda_309@hotmail.com>
2016-03-11 01:51:57 -0500

    Finsih business sign up

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,4 +1,28 @@
 class BusinessesController < ApplicationController
-  def new
+  
+  def show
+  	@business = Business.find(params[:id])
   end
+
+  def new
+  	@business = Business.new
+  end
+
+  def create
+  	@business = Business.new(business_params)
+  	if@business.save
+  		flash[:success] = "Welcome to Futbol Finder!"
+  		redirect_to @business
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  def business_params
+  	params.require(:business).permit(:name, :email, :phone, :address, :city, :state, :zipcode, :password, 
+  										:password_confirmation)
+  end
+
 end
