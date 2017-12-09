e99a50d3d017501925f9162abd63f34d67c88716
luis Fonseca <luisfonseca@luiss-Mac-mini-2.local>
2017-12-09 11:16:26 -0800

    change schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209073812) do
+ActiveRecord::Schema.define(version: 20171209191444) do
 
   create_table "businesses", force: :cascade do |t|
     t.string   "name",            limit: 255
